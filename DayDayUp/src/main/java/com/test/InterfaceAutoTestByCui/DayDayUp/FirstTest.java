52891d99516055afc12ca0c22a64d99b1d3aea7e
bjcuizheqian <bjcuizheqian@corp.netease.com>
2018-05-29 19:43:08 +0800

    test

---
+++ DayDayUp/src/main/java/com/test/InterfaceAutoTestByCui/DayDayUp/FirstTest.java
@@ -0,0 +1,15 @@
+package com.test.InterfaceAutoTestByCui.DayDayUp;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	
+	@Test
+	public void check(){
+		
+		System.out.println("test");
+		
+    }
+	
+
+}
